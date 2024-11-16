e3586cc3e5232272ecc9f092ccd880fa35e45265
wrblr <wrbell@uchicago.edu>
2024-11-16 10:40:12 +0000

    directors functioning

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,42 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def update
+    #Get the params out of id
+    direct_id = params.fetch("the_id")
+    #Look up the existing record
+    matching_records = Director.where({ :id => direct_id })
+    the_director = matching_records.at(0)
+    #Overwrite each column with the values from user inputs
+    the_director.name = params.fetch("the_name")
+    the_director.dob = params.fetch("the_dob")
+    the_director.bio = params.fetch("the_bio")
+    the_director.image = params.fetch("the_image")
+    #Save the new inputs
+    the_director.save
+    #Redirect
+    redirect_to("/directors/#{the_director.id}")
+  end
+
+  def create
+    direct = Director.new
+    direct.name = params.fetch("the_name")
+    direct.dob = params.fetch("the_dob")
+    direct.bio = params.fetch("the_bio")
+    direct.image = params.fetch("the_image")
+    
+    direct.save
+
+    redirect_to("/directors")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_records = Director.where({ :id => the_id})
+    the_director = matching_records.at(0)
+    the_director.destroy
+    
+    redirect_to("/directors")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  post("/insert_director_record", { :controller => "directors", :action => "create" })
+  get("/delete_director/:an_id", { :controller => "directors", :action => "destroy" })
+  post("/modify_director_record/:the_id", { :controller => "directors", :action => "update" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
