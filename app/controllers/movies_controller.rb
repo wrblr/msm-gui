f7d81d0fca71117ae489e1da6449aa10c5086936
wrblr <wrbell@uchicago.edu>
2024-11-16 05:33:31 +0000

    directors and movies no edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,34 @@
 class MoviesController < ApplicationController
+  def create
+    # params look like this:
+    #{"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"7", "controller"=>"movies", "action"=>"create"}
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+    
+    m.save
+
+    redirect_to("/movies")
+    #Retreive user inputs from params
+    #Create a record in the movie table
+    #Populate each column with user input
+    #Save
+
+    #Redirect user to movies URL
+  end
+  
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_records = Movie.where({ :id => the_id})
+    the_movie = matching_records.at(0)
+    the_movie.destroy
+    
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
